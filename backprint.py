33ee2cc50058b1ad89c3d5ae7865c1139f5d27c8
c0d3d57 <c0d3d@hotmail.com>
2014-11-17 09:46:21 +0100

    Create backprint.py
    A simple python function to print any line/thing backword.

---
+++ backprint.py
@@ -0,0 +1,30 @@
+# A simple function to print anything backwords
+
+import sys
+
+# Here we authomatically recall the function anytime a job is done.
+def redo():
+	mes()
+
+# Main workflow
+def mes():
+	message = raw_input('Enter word > ')
+	# Type exit to opt out of the program
+	if "exit" in message:
+		sys.exit()
+	
+	translated = ''
+	trans = []
+
+	i = len(message) - 1
+	while i >= 0:
+		translated = translated + message[i]
+		i = i - 1
+		# Wer store our outputs in an array in order not to get a wrong result.
+		trans.append(translated)
+	
+	print(translated)
+	redo()	
+
+# Call the main function
+mes()
